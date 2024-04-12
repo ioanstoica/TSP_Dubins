adb2f474fdd5063879e6aa943213744533fb1689
ioanstoica <ioanstoica89@gmail.com>
2024-04-12 20:09:30 +0300

    Refactor TSP_Dubins function to separate solving TSP and generating Dubins path

--- main.py
+++ main.py
@@ -20,18 +20,35 @@ def solveTSP(Wptz, r):
                 row.append(dubins_length(param))
         distance_matrix.append(row)
     
-    print(distance_matrix)
     distance_matrix = np.array(distance_matrix)
     # NU vrem sa ne intoarcem la punctul de start
     distance_matrix[:, 0] = 0
 
     # Solve the TSP problem
     permutation, distance = solve_tsp_dynamic_programming(distance_matrix)
-    print(permutation, distance)
-
 
     Wptz = [Wptz[i] for i in permutation]
-    return Wptz
+    return Wptz, distance
+
+# Rezolvă problema TSP, reordonând punctele pentru a minimiza lungimea traseului
+def TSP_Dubins(Wptz, r, ax):
+    Wptz, total_length = solveTSP(Wptz, r)
+
+    # Calculează traseul Dubins
+    for i in range(len(Wptz)-1):
+        param = calcDubinsPath(Wptz[i], Wptz[i+1], r, 20)
+        path = dubins_traj(param,1)
+
+        # Plot the results
+        ax.plot(Wptz[i].x,Wptz[i].y,'kx')
+        ax.plot(Wptz[i+1].x,Wptz[i+1].y,'kx')
+        ax.plot(path[:,0],path[:,1],'b-')
+
+    ax.grid(True)
+    ax.axis("equal")
+    ax.set_title('Best trajectory, length: ' + str(round(total_length)) + 'm' + ', radius: ' + str(r) + 'm')
+    ax.set_xlabel(' ')
+    ax.set_ylabel('Y')
 
 def main():
     # User's waypoints: [x, y, heading (degrees)] - heading is the angle of the vehicle's orientation where 0 is North
@@ -41,44 +58,14 @@ def main():
             Waypoint(-5000,5000,270), 
             Waypoint(0,10000,0)]
 
+    # Define the turning radius, try for different values
     radius = [50, 100, 150, 200]
-
 
     fig, axs = plt.subplots(len(radius), 1, figsize=(10, 12))
 
+    # Pentru fiecare rază de virare, rezolvă problema TSP și generează traseul Dubins
     for t in range(len(radius)):
-        # Rezolvă problema TSP, reordonând punctele pentru a minimiza lungimea traseului
-        r = radius[t]
-        Wptz = solveTSP(Wptz, r)
-
-        # Calculează traseul Dubins
-        i = 0
-        total_length = 0  # Inițializează o variabilă pentru a stoca lungimea totală a traseului
-        while i<len(Wptz)-1:
-            param = calcDubinsPath(Wptz[i], Wptz[i+1], r, 20)
-            path = dubins_traj(param,1)
-
-            # Calculează lungimea totală a traseului
-            length = dubins_length(param)
-            total_length += length  # Adaugă lungimea segmentului curent la lungimea totală
-            print('Segment Length: ',length)
-    
-
-            # Plot the results
-            axs[t].plot(Wptz[i].x,Wptz[i].y,'kx')
-            axs[t].plot(Wptz[i+1].x,Wptz[i+1].y,'kx')
-            axs[t].plot(path[:,0],path[:,1],'b-')
-            i+=1
-
-        # Afișează lungimea totală a traseului
-        print("Lungimea totală a drumului Dubins este:", total_length, "metri")
-
-        axs[t].grid(True)
-        axs[t].axis("equal")
-        axs[t].set_title('Best trajectory, length: ' + str(round(total_length)) + 'm' + ', radius: ' + str(r) + 'm')
-        axs[t].set_xlabel('X')
-        axs[t].set_ylabel('Y')
-        axs[t].legend()
+        TSP_Dubins(Wptz, radius[t], axs[t])
 
     plt.tight_layout()    
     plt.show()
